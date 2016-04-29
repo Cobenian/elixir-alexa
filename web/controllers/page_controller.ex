3d22c53f0ee6347a0d696cea37b2c119b0455440
Bryan Weber <bryan@cobenian.com>
2016-04-29 15:50:50 -0400

    adding a few different sayings

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -5,12 +5,7 @@ defmodule Alexa.PageController do
     render conn, "index.html"
   end
 
-  def alexa(conn, params) do
-    IO.puts "called with:"
-    IO.inspect params
-
-    response = 
-
+  def process_request(conn, %{"request" => %{"intent" => %{"name" => "Mastery"}}}) do
     %{
 	  "version" => "1.0",
 	  "response" => %{
@@ -18,11 +13,43 @@ defmodule Alexa.PageController do
 	      "type" => "PlainText",
 	      "text" => "Elixir is awesome",
 	    },
-	    "shouldEndSession" => true 
-	  }
+	    "shouldEndSession" => true
+	   }
     }
-    |> IO.inspect
+  end
 
-    json conn, response 
+  def process_request(conn, %{"request" => %{"intent" => %{"name" => "Ivan"}}}) do
+    %{
+    "version" => "1.0",
+    "response" => %{
+      "outputSpeech" => %{
+        "type" => "PlainText",
+        "text" => "Elixir is awesome, but you are more awesome.",
+      },
+      "shouldEndSession" => true
+     }
+    }
+  end
+
+  def process_request(conn, params) do
+    %{
+    "version" => "1.0",
+    "response" => %{
+      "outputSpeech" => %{
+        "type" => "PlainText",
+        "text" => "Dude, I didn't understand that at all, but I still love Elixir.",
+      },
+      "shouldEndSession" => true
+     }
+    }
+  end
+
+  def alexa(conn, params) do
+    IO.puts "called with:"
+    IO.inspect params
+
+    response = process_request(conn, params) |> IO.inspect
+
+    json conn, response
   end
 end
